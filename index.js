36ee2898fb97c6450f228d17e652ea94011accfe
ViProzTTG <52833336+ViProzTTG@users.noreply.github.com>
2019-08-12 14:30:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 var prefix = "T!";
 
-client.login("");
+client.login("process.env.BOT_TOKEN");
 
 client.on("message", message =>{
     if(!message.guild) return
